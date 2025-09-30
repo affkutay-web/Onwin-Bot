415b40e573a22b2d5b11f29a3e5acc5b43f4a1c4
OWbot <affkutay@gmail.com>
2025-09-30 15:32:23 +0300

    Delete firebase_config.js

--- firebase_config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAHHHK7PJQdYRGulWwMie3mwpI-Rnat8Og",
-  authDomain: "onwin-bot.firebaseapp.com",
-  projectId: "onwin-bot",
-  storageBucket: "onwin-bot.firebasestorage.app",
-  messagingSenderId: "921330264402",
-  appId: "1:921330264402:web:c2a7ae36ec28b4e8c4328b",
-  measurementId: "G-MFXVH1B9E4"
-};
